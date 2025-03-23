93d7b92f8aa185972f7d9a2cfd160b831652347b
Knight1357 <lius484441475@gmail.com>
2025-03-23 17:17:02 +0800

    Update logo and web title

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router";
 
 import { useTheme } from "@/theme/hooks";
 
-import { Iconify } from "../icon";
+// import { Iconify } from "../icon";
 
 interface Props {
 	size?: number | string;
@@ -12,7 +12,11 @@ function Logo({ size = 50 }: Props) {
 
 	return (
 		<NavLink to="/">
-			<Iconify icon="solar:code-square-bold" color={themeTokens.color.palette.primary.default} size={size} />
+			<img
+				src="src\assets\images\logo.svg" // 替换为你的 SVG 文件路径
+				alt="Logo"
+				style={{ width: size, height: size, color: themeTokens.color.palette.primary.default }}
+			/>
 		</NavLink>
 	);
 }
